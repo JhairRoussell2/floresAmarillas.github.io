d0a2074df498f4052d2ba363b10e05b9f9067b98
JhairRoussell2 <jhairrmb3@gmail.com>
2024-09-21 22:32:00 -0500

    Initial commit

---
+++ anim.js
@@ -0,0 +1,55 @@
+// Sincronizar las letras con la canción
+var audio = document.querySelector("audio");
+var lyrics = document.querySelector("#lyrics");
+
+// Array de objetos que contiene cada línea y su tiempo de aparición en segundos
+var lyricsData = [
+  { text: "Él la estaba esperando con una flor amarilla", time: 16 },
+  { text: "Ella lo estaba soñando con la luz en su pupila", time: 24 },
+  { text: "Y el amarillo de sol iluminaba la esquina", time: 31 },
+  { text: "Lo sentía tan cercano, lo sentía desde niña", time: 40 },
+  { text: "Ella sabía que él sabía, Que algún día pasaría, Que vendría a buscarla", time: 46 },
+  { text: "Con sus flores amarillas", time: 51 },
+  { text: "No te apures, no detengas, El instante del encuentro. Está dicho que es un hecho", time: 59 },
+  { text: "No la pierdas, no hay derecho, No te olviiiiiides, Que la vida", time: 65 },
+  { text: "Casi nunca está dormidaaaaaaaaa", time: 71 },
+
+];
+
+// Animar las letras
+function updateLyrics() {
+  var time = Math.floor(audio.currentTime);
+  var currentLine = lyricsData.find(
+    (line) => time >= line.time && time < line.time + 6
+  );
+
+  if (currentLine) {
+    // Calcula la opacidad basada en el tiempo en la línea actual
+    var fadeInDuration = 0.1; // Duración del efecto de aparición en segundos
+    var opacity = Math.min(1, (time - currentLine.time) / fadeInDuration);
+
+    // Aplica el efecto de aparición
+    lyrics.style.opacity = opacity;
+    lyrics.innerHTML = currentLine.text;
+  } else {
+    // Restablece la opacidad y el contenido si no hay una línea actual
+    lyrics.style.opacity = 0;
+    lyrics.innerHTML = "";
+  }
+}
+
+setInterval(updateLyrics, 1000);
+
+//funcion titulo
+// Función para ocultar el título después de 216 segundos
+function ocultarTitulo() {
+  var titulo = document.querySelector(".titulo");
+  titulo.style.animation =
+    "fadeOut 3s ease-in-out forwards"; /* Duración y función de temporización de la desaparición */
+  setTimeout(function () {
+    titulo.style.display = "none";
+  }, 3000); // Espera 3 segundos antes de ocultar completamente
+}
+
+// Llama a la función después de 216 segundos (216,000 milisegundos)
+setTimeout(ocultarTitulo, 216000);
